d138ef3e9948f8189f7f4276adefc8bde552e06d
lorenz <looooo@users.noreply.github.com>
2022-04-28 13:11:51 +0200

    add python executable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ cd build
 
 cmake -G "Ninja" ^
     -D CMAKE_BUILD_TYPE="Release" ^
+    -D PYTHON_EXECUTABLE:FILEPATH=%PREFIX%/python
     -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
     ..
 
