5c06120554128598a372ac68b072a6b0c76deb3d
lorenz <looooo@users.noreply.github.com>
2022-04-29 00:21:53 +0200

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,8 @@ cd build
 
 cmake -G "Ninja" ^
     -D CMAKE_BUILD_TYPE="Release" ^
-    -D PYTHON_EXECUTABLE:FILEPATH=%PREFIX%/python ^
     -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
+    -D Python_EXECUTABLE:FILEPATH=%PREFIX%/python ^
     ..
 
 
