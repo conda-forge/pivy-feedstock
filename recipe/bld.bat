dd6f8aa267d50cbf46098d4c794e9e263591259a
lorenz <looooo@users.noreply.github.com>
2024-10-07 08:11:51 +0200

    Qt6 py312 (#44)
    * add python3.12 migration
    
    * update meta.yaml to use qt6 with py312
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.09.34.15
    
    * update sha256sum
    
    * use qt=6.6
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.09.34.15
    
    * update build-scripts
    
    * update sha256sum
    
    * macosx specifications
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.16.08.11
    
    * QT_HOST_PATH
    
    * cross-compile
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.16.08.11
    
    * revert linux-aarch64 to emulated
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.16.08.11
    
    * Add mesa-libGL to yum requirements
    
    * MNT: Re-rendered with conda-build 3.27.0, conda-smithy 3.30.0, and conda-forge-pinning 2023.11.26.16.08.11
    
    * add osx runtime requirement
    
    * add pin_compatible for soqt and coin
    
    * fix linter-message
    
    * 0.6.9.a0
    
    * Update sha256sum
    
    * Update to 0.6.9
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.25.08.12.52
    
    * fixes for linter
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.25.08.12.52
    
    * remove macosx deploment target
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.25.08.12.52
    
    * qt6.7
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.25.08.12.52
    
    * use clang for windows
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.25.09.41.27
    
    * simplify build-scripts
    
    * fix
    
    * build entire matrix for qt5 and qt6
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.27.17.25.06
    
    * add missing build_variant
    
    * use other syntax for build_variant
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.27.17.25.06
    
    * skip py312 for qt5
    
    * simplify selectors
    
    * python_include_dir
    
    * delete migration files
    
    * manually remove ci-files for py312qt5
    
    * delete qt5_py312
    
    * delete qt5_py312
    
    * Cross compile linux-aarch64
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.41.1, and conda-forge-pinning 2024.10.06.14.01.57
    
    * remove some yum requirements
    
    * remove all yum-requirements
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.41.1, and conda-forge-pinning 2024.10.07.03.45.12
    
    ---------
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,7 @@
-mkdir build
-cd build
-
-cmake -G "Ninja" ^
+cmake -G "Ninja" -B build -S . ^
     -D CMAKE_BUILD_TYPE="Release" ^
     -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
     -D Python_EXECUTABLE:FILEPATH=%PREFIX%/python ^
-    ..
+    -D PIVY_USE_QT6:BOOL=%USE_QT6%
 
-
-ninja install
+ninja -C build install
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,18 @@
-mkdir -p build
-cd build
+echo "USE_QT6 is set ${USE_QT6}"
 
-if [[ ${HOST} =~ .*linux.* ]]; then
+if [[ ${HOST} =~ .*linux.* && ${USE_QT6} = "0" ]]; then
   sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
   sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
 fi
 
-PIVY_CPP_FLAGS='-std=c++1z '
+Python_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
 
-cmake ${CMAKE_ARGS} \
-    -G "Ninja" \
-    -D CMAKE_BUILD_TYPE="Release" \
-    -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-    -D Python_EXECUTABLE="$PYTHON" \
-    ..
+cmake -G "Ninja" -B build \
+      -D CMAKE_BUILD_TYPE:STRING="Release" \
+      -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+      -D Python_EXECUTABLE:PATH="$PYTHON" \
+      -D Python_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR} \
+      -D PIVY_USE_QT6:BOOL=$USE_QT6 \
+      -D QT_HOST_PATH:PATH="${PREFIX}"
 
-ninja install
+ninja -C build install
